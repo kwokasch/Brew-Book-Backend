401946072bcd2e052561759756b9e6aac99a565c
github username <github email address>
2019-10-18 10:32:47 -0600

    migrate heroku db

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,9 +1,9 @@
 class Beer < ApplicationRecord
   # belongs_to :brewery
 
-  validates :name, presence: true, uniqueness: {case_sensitive: true}
+  validates :name, presence: true, uniqueness: true
   validates :variety, presence: true
   validates :rating, numericality: {only_integer: true, less_than_or_equal_to: 5} 
-  validates :comments
+  # validates :comments, presence: true
   # validates :brewery_id, presence: true
 end
