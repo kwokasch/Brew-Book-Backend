6eb39a97ba186d7af60520d1f9e459ee4a7da4e1
github username <github email address>
2019-10-18 11:47:10 -0600

    add firebase URL to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://brew-diary.firebaseapp.com'
 
     resource '*',
       headers: :any,
